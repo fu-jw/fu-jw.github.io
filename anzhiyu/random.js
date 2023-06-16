863aece6cb4a9688a7de86ca0bceaa724640c5ab
fu-jw <1151242757@qq.com>
2023-06-16 08:29:05 +0000

    新增文章:日志体系 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/63166764.html","posts/e317821c.html","posts/f026602.html","posts/4ccc97e8.html","posts/ab942f3d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/63166764.html","posts/e317821c.html","posts/f026602.html","posts/4ccc97e8.html","posts/ab942f3d.html","posts/c3b98719.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
