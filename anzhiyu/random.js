4dffc442df7143aeec0790b8026afbe400906c21
fu-jw <1151242757@qq.com>
2023-06-30 14:19:00 +0000

    新增文章:AList+云盘+LskyPro 搭建大容量图床 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/c3b98719.html","posts/63166764.html","posts/e317821c.html","posts/f026602.html","posts/4ccc97e8.html","posts/4275e528.html","posts/ab942f3d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4140de30.html","posts/c3b98719.html","posts/63166764.html","posts/e317821c.html","posts/f026602.html","posts/4ccc97e8.html","posts/4275e528.html","posts/ab942f3d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
